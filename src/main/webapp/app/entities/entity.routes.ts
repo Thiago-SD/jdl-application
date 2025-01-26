06b8b7ce3a256da33cbe2fbb178272a6d2bd9ec3
AwsumName03 <th.damasceno@hotmail.com>
2025-01-25 23:10:24 -0300

    Initial Commit

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,46 @@ const routes: Routes = [
     data: { pageTitle: 'jdlApplicationApp.adminAuthority.home.title' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'region',
+    data: { pageTitle: 'jdlApplicationApp.region.home.title' },
+    loadChildren: () => import('./region/region.routes'),
+  },
+  {
+    path: 'country',
+    data: { pageTitle: 'jdlApplicationApp.country.home.title' },
+    loadChildren: () => import('./country/country.routes'),
+  },
+  {
+    path: 'location',
+    data: { pageTitle: 'jdlApplicationApp.location.home.title' },
+    loadChildren: () => import('./location/location.routes'),
+  },
+  {
+    path: 'department',
+    data: { pageTitle: 'jdlApplicationApp.department.home.title' },
+    loadChildren: () => import('./department/department.routes'),
+  },
+  {
+    path: 'task',
+    data: { pageTitle: 'jdlApplicationApp.task.home.title' },
+    loadChildren: () => import('./task/task.routes'),
+  },
+  {
+    path: 'employee',
+    data: { pageTitle: 'jdlApplicationApp.employee.home.title' },
+    loadChildren: () => import('./employee/employee.routes'),
+  },
+  {
+    path: 'job',
+    data: { pageTitle: 'jdlApplicationApp.job.home.title' },
+    loadChildren: () => import('./job/job.routes'),
+  },
+  {
+    path: 'job-history',
+    data: { pageTitle: 'jdlApplicationApp.jobHistory.home.title' },
+    loadChildren: () => import('./job-history/job-history.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
